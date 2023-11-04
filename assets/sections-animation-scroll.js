c2d6f2c009a92d715dce6219b34035d363543c71
Miske <miskelvilaly@gmail.com>
2023-11-04 22:47:39 +0000

    ADDED THE FIRST DESCRITPTION SECTION

--- assets/sections-animation-scroll.js
+++ assets/sections-animation-scroll.js
@@ -4,13 +4,35 @@ class SectionsAnimationScroll extends HTMLElement {
         super();
         this.initSectionHero()
         this.initScroll();
+        this.initSectionFirstDescription();
     }
+
     initScroll(){
-        this.addEventListener('scroll', () => {
-            console.log("window.innerHeight: " + this.offsetHeight)
-            console.log("document.documentElement.scrollTop: " + this.scrollTop)   
+        
+        let break_point_first_section_container = (window.innerHeight * 4) ;
+        let first_section_container = this.querySelector(".first_description_container")
+        let white_overlay_video = first_section_container.querySelector(".text_description_animation_container");
+        let text_white_overlay = white_overlay_video.querySelector("h1");
+        window.addEventListener('scroll', () => {
+            if(document.documentElement.scrollTop > (window.innerHeight * 1) && document.documentElement.scrollTop <= (window.innerHeight * 4)){
+                first_section_container.style.background = "black";
+                first_section_container.style.marginTop = "calc(" + Math.min( document.documentElement.scrollTop - window.innerHeight,break_point_first_section_container) + "px - 6.74vw)";
+                white_overlay_video.style.opacity = (1 - (document.documentElement.scrollTop/window.innerHeight * 8)) ;
+                text_white_overlay.style.transform = "scale(" + document.documentElement.scrollTop % window.innerHeight * 5   +")"
+
+            }else if(document.documentElement.scrollTop <= (window.innerHeight * 1)){
+                first_section_container.style.marginTop = "0px"
+            }
+
+            // if(document.documentElement.scrollTop > (window.innerHeight * 4) || document.documentElement.scrollTop < window.innerHeight){
+            //     first_section_container.style.position = "default"
+            //     let first_section_container = this.querySelector(".first_description_container")
+            //     first_section_container.style.background = "black";
+
+            // }
         });
     }
+
     initSectionHero(){
         let hero_button = this.querySelector(".play_video_hero_btn");
         let video_hero_element = this.querySelector(".video_container");
@@ -24,6 +46,9 @@ class SectionsAnimationScroll extends HTMLElement {
             video_hero_element.style.display = "none";
             video_iframe.src = "https://www.youtube.com/embed/WE155WwKNkk?&controls=0&loop=1&disablekb=1&playsinline=1&cc_load_policy=0&cc_lang_pref=auto&widget_referrer=https%3A%2F%2Fglobal.dreametech.com%2Fproducts%2Fl20-ultra&rel=0&showinfo=1&iv_load_policy=3&modestbranding=1&customControls=false&noCookie=false&enablejsapi=1&origin=https%3A%2F%2Fglobal.dreametech.com&widgetid=1"
         })
+    }
+    initSectionFirstDescription(){
+        
     }
 }
 customElements.define('sections-animation-scroll', SectionsAnimationScroll);
